022fb6e75da12d6d8d85c8992f1a8812428ec3bb
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-07 10:23:10 +0530

    Add files via upload

---
+++ SameLetterRow.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class SameLetterRow {
+public static void main(String[]args){
+    Scanner kavi = new Scanner(System.in);
+    int k = kavi.nextInt();
+    for(int i=1;i<=k;i++){
+        for(int j = 1;j<=i;j++ ){
+            System.out.print((char)('A' + i-1)+ " ");
+        }
+        System.out.println();
+    }
+}    
+}
