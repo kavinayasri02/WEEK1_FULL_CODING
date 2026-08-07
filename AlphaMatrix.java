94c55c7eabe1a38fed253374c66838dfd5cf7557
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-07 10:18:35 +0530

    Add files via upload

---
+++ AlphaMatrix.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AlphaMatrix {
+    public static void main(String[]args){
+    Scanner kavi = new Scanner(System.in);
+    int k = kavi.nextInt();
+    for(int i=1;i<=k;i++){
+        for(int j = 1;j<=k;j++ ){
+            System.out.print((char)('A' + i-1)+ " ");
+        }
+        System.out.println();
+}
+    }
+}
