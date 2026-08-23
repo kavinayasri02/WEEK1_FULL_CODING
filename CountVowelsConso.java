5cc1b5c45dc6ddbdb8bb5ebf34d54773fcc32084
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-23 15:27:48 +0530

    Update CountVowelsConso.java

--- CountVowelsConso.java
+++ CountVowelsConso.java
@@ -20,3 +20,34 @@ public class CountVowelsConso{
         System.out.println("Count of Consonants are : " + ConsoCount);
     }
 }
+
+Using HashSet Approach
+
+import java.util.HashSet;
+import java.util.Scanner;
+public class CountVowelsConso{
+    public static void main(String[]args){
+        Scanner kavi = new Scanner(System.in);
+        String S = kavi.nextLine().toLowerCase();
+        HashSet<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+        int vowelCount = 0;
+        int consoCount = 0;
+        for(int i = 0;i<S.length();i++){
+            char ch = S.charAt(i);
+            if(ch >= 'a' && ch <= 'z' ){
+                if(vowels.contains(ch)){
+                    vowelCount++;
+                }else{
+                    consoCount++;
+                }
+            }
+        }
+        System.out.println("Vowels : " + vowelCount);
+        System.out.println("Consonants :" + consoCount);
+    }
+}
