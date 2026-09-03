130d4d2dc06ff5820630f09f6bd4bee86d5c47f6
Kavinayasri J <kavinayasri02@gmail.com>
2026-09-03 16:18:21 +0530

    Add files via upload

---
+++ ElematOddIndex.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ElematOddIndex {
+    public static void main(String[]args){
+        Scanner kavi = new Scanner(System.in);
+        int k = kavi.nextInt();
+        int[]arr = new int[k];
+        for(int i =0;i<k;i++){
+            arr[i] = kavi.nextInt();
+        }
+        for(int i =0;i<k;i++){
+            if(i % 2 != 0){
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
