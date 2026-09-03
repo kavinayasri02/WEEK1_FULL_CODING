507c19bb939b5558e3d484e18dcd471d732a748f
Kavinayasri J <kavinayasri02@gmail.com>
2026-09-03 16:26:00 +0530

    Add files via upload

---
+++ SumofOdd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SumofOdd {
+    public static void main(String[] args) {
+        Scanner kavi = new Scanner(System.in);
+        int k = kavi.nextInt();
+        int[] arr = new int[k];
+        for(int i =0;i<k;i++){
+            arr[i] = kavi.nextInt();
+        }
+        int sum = 0;
+        for(int i =0;i<k;i++){
+            if(arr[i]%2 !=0){
+                sum = sum + arr[i];
+            }
+        }
+        System.out.print(sum);
+    }
+}
