74615e2519f1b27b529a91fe19f0b24390dbff48
Kavinayasri J <kavinayasri02@gmail.com>
2026-09-26 13:37:11 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -3,21 +3,21 @@ public class Armstrong {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
         System.out.println("Enter a number");
-        int number = num.nextInt();
+        int n = num.nextInt();
         num.close();
-        int originalnumber = number;
+        int og = n;
         int result = 0;
-        int n =String.valueOf(number).length();
-        while(number>0){
-            int digit=number%10;
+        int n =String.valueOf(n).length();
+        while(n>0){
+            int digit=n%10;
             result+=(int)Math.pow(digit,n);
-            number/=10;
+            n/=10;
         }
-       if(result == originalnumber){
-        System.out.println(originalnumber + "is an Armstrong number");
+       if(result == og){
+        System.out.println(og + "is an Armstrong number");
        }
        else{
-        System.out.println(originalnumber + "is not an Armstrong number"); 
+        System.out.println(og + "is not an Armstrong number"); 
        }
     }
 }
